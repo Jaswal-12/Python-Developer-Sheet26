b887e03500d10c116f7ba0a38a627ff3ce7f3aa9
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-01-11 11:29:27 -0800

    Enhance FirstProgram.py with print examples
    Added examples of printing and escape sequences in Python.

--- Basics/FirstProgram.py
+++ Basics/FirstProgram.py
@@ -1,1 +1,22 @@
 
+#How we print our program in python
+print("Hello world")
+print(5)
+print("Bye")
+
+
+
+#Escape sequence and print in python
+print("karan jaswal")
+#if  i want a new line after karan so we use /n)
+
+print("karan \n jaswal")
+
+#output -> 
+karan 
+jaswal
+
+print("Hey",5,6,sep="-",end="009\n")
+#output
+Hey-5-6009
+
